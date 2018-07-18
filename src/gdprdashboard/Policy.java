a363c604bf9781c3510a556f42fe422258cc20bf
arsho <shovon.sylhet@gmail.com>
2018-07-18 20:38:37 +0600

    7 added skeleton classes with class variables and methods

--- src/gdprdashboard/Policy.java
+++ src/gdprdashboard/Policy.java
@@ -1,6 +1,38 @@
 package gdprdashboard;
 
-public class Policy{
-    String policyName;
-    String policyDescription;
+import java.util.UUID;
+
+public class Policy {
+
+    // Todo : check if we can store default value
+    private UUID id;
+    private String name;
+    private String description;
+
+    public Policy(String name, String description) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
