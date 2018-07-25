5f0892945340f057696a292febab32485301c75f
arsho <shovon.sylhet@gmail.com>
2018-07-25 12:45:41 +0600

    #9 Updated GDPR Dashboard architecture

--- src/gdprdashboard/GDPRDashboard.java
+++ src/gdprdashboard/GDPRDashboard.java
@@ -3,44 +3,73 @@ package gdprdashboard;
 import java.util.ArrayList;
 
 public class GDPRDashboard {
-
+    
     public static void main(String[] args) {
+        ArrayList<Policy> policies = new ArrayList<Policy>();
+        ArrayList<User> users = new ArrayList<User>();
+        ArrayList<UserDashboard> userDashboards = new ArrayList<UserDashboard>();
+        
         Policy googleAnalytics = new Policy("Google Analytics", "User data is being sent to Google Analytics");
         Policy facebookGraphApi = new Policy("Facebook Graph API", "User data is being sent to Facebook Graph API");
-
+        
+        policies.add(googleAnalytics);
+        policies.add(facebookGraphApi);
+        
         User shovon = new User("Ahmedur Rahman Shovon", "arshovon@cefalo.com", "Bangladesh");
         User arnab = new User("Arnab Kumar Shil", "arnab@cefalo.com", "Bangladesh");
-
-        PolicyMapper shovonGA = new PolicyMapper(googleAnalytics, true);
-        PolicyMapper shovonFB = new PolicyMapper(facebookGraphApi, false);
-
-        PolicyMapper arnabGA = new PolicyMapper(googleAnalytics, false);
-        PolicyMapper arnabFB = new PolicyMapper(facebookGraphApi, true);
-
-        Dashboard shovonDashboard = new Dashboard(shovon);
-        shovonDashboard.addPolicyMapper(shovonGA);
-        shovonDashboard.addPolicyMapper(shovonFB);
-
-        Dashboard arnabDashboard = new Dashboard(arnab);
-        arnabDashboard.addPolicyMapper(arnabGA);
-        arnabDashboard.addPolicyMapper(arnabFB);
-
-        ArrayList<PolicyMapper> shovonPolicies = shovonDashboard.getPolicyMapperList();
-        System.out.println(shovon.getName() + "'s GDPR consents:");
-        for (PolicyMapper policyMapper : shovonPolicies) {
-            String policyName = policyMapper.getPolicy().getName();
-            boolean userChoice = policyMapper.getUserChoice();
-            System.out.println(policyName + "-->" + userChoice);
+        
+        users.add(shovon);
+        users.add(arnab);
+        
+        UserDashboard shovonDashboard = new UserDashboard(shovon.getId(), policies);
+        UserDashboard arnabDashboard = new UserDashboard(arnab.getId(), policies);
+        
+        userDashboards.add(shovonDashboard);
+        userDashboards.add(arnabDashboard);
+        
+        System.out.println("===========================================================");
+        for (UserDashboard userDashboard : userDashboards) {
+            System.out.println("Showing dashboard for user:" + userDashboard.getUserId());
+            for (PolicyMapper policyMapper : userDashboard.getPolicyMappers(policies)) {
+                System.out.println("Policy " + policyMapper.getPolicyId() + "-->" + policyMapper.getUserChoice());
+            }
         }
-
-        ArrayList<PolicyMapper> arnabPolicies = arnabDashboard.getPolicyMapperList();
-        System.out.println(arnab.getName() + "'s GDPR consents:");
-        for (PolicyMapper policyMapper : arnabPolicies) {
-            String policyName = policyMapper.getPolicy().getName();
-            boolean userChoice = policyMapper.getUserChoice();
-            System.out.println(policyName + "-->" + userChoice);
+        System.out.println("===========================================================\n");
+        
+        userDashboards.get(1).updatePolicyMappers(policies, facebookGraphApi.getId(), false);
+        
+        System.out.println("===========================================================");
+        for (UserDashboard userDashboard : userDashboards) {
+            System.out.println("Showing dashboard for user:" + userDashboard.getUserId());
+            for (PolicyMapper policyMapper : userDashboard.getPolicyMappers(policies)) {
+                System.out.println("Policy " + policyMapper.getPolicyId() + "-->" + policyMapper.getUserChoice());
+            }
         }
-
+        System.out.println("===========================================================\n");
+        
+        Policy netsPaymentApi = new Policy("Nets Payment API", "User data is being sent to Nets Payment API");
+        policies.add(netsPaymentApi);
+        
+        System.out.println("===========================================================");
+        for (UserDashboard userDashboard : userDashboards) {
+            System.out.println("Showing dashboard for user:" + userDashboard.getUserId());
+            for (PolicyMapper policyMapper : userDashboard.getPolicyMappers(policies)) {
+                System.out.println("Policy " + policyMapper.getPolicyId() + "-->" + policyMapper.getUserChoice());
+            }
+        }
+        System.out.println("===========================================================\n");
+        
+        policies.remove(googleAnalytics);
+        
+        System.out.println("===========================================================");
+        for (UserDashboard userDashboard : userDashboards) {
+            System.out.println("Showing dashboard for user:" + userDashboard.getUserId());
+            for (PolicyMapper policyMapper : userDashboard.getPolicyMappers(policies)) {
+                System.out.println("Policy " + policyMapper.getPolicyId() + "-->" + policyMapper.getUserChoice());
+            }
+        }
+        System.out.println("===========================================================\n");
+        
     }
-
+    
 }
---
+++ src/gdprdashboard/UserDashboard.java
@@ -0,0 +1,78 @@
+package gdprdashboard;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class UserDashboard {
+
+    private UUID id;
+    private UUID userId;
+    private ArrayList<PolicyMapper> policyMappers;
+
+    public UserDashboard(UUID userId, ArrayList<Policy> policies) {
+        this.userId = userId;
+        this.policyMappers = new ArrayList<PolicyMapper>();
+        for (Policy policy : policies) {
+            PolicyMapper policyMapper = new PolicyMapper(policy.getId(), policy.getDefaultValue());
+            this.policyMappers.add(policyMapper);
+        }
+    }
+
+    private void syncPolicyMappers(ArrayList<Policy> policies) {
+        // Add new policy to policy mapper list with policies default value
+        for (Policy policy : policies) {
+            boolean existingPolicy = false;
+            for (PolicyMapper policyMapper : this.policyMappers) {
+                if (policyMapper.getPolicyId() == policy.getId()) {
+                    existingPolicy = true;
+                }
+            }
+            if (existingPolicy == false) {
+                this.policyMappers.add(new PolicyMapper(policy.getId(), policy.getDefaultValue()));
+            }
+        }
+        // Remove deleted policie from policy mapper list
+        for (PolicyMapper policyMapper : new ArrayList<PolicyMapper>(policyMappers)) {
+            boolean foundPolicy = false;
+            for (Policy policy : policies) {
+                if (policyMapper.getPolicyId() == policy.getId()) {
+                    foundPolicy = true;
+                }
+            }
+            if (foundPolicy == false) {
+                policyMappers.remove(policyMapper);
+            }
+        }
+    }
+
+    public void updatePolicyMappers(ArrayList<Policy> policies, UUID policyId, boolean userChoice) {
+        syncPolicyMappers(policies);
+        for (PolicyMapper policyMapper : this.policyMappers) {
+            if (policyMapper.getPolicyId() == policyId) {
+                policyMapper.setUserChoice(userChoice);
+            }
+        }
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public ArrayList<PolicyMapper> getPolicyMappers(ArrayList<Policy> policies) {
+        syncPolicyMappers(policies);
+        return policyMappers;
+    }
+
+}
